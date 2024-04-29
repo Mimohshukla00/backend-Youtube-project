7df41f3084294d3ce49bd95a734ed1058e188918
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-30 00:58:41 +0530

    new commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const PORT= process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// connection if db
+
+require("./db/database").connectDB()
+app.listen(PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
+app.get("/", (req, res) => {
+  // console.log(req.ip)
+  // console.log(req.ips)
+  // console.log(req.originalUrl)
+  // console.log(req.params)
+  // console.log(req.path)
+  console.log(req.xhr)
+  res.send("hello world");
+});
