bc2aff0d412f91ef326fad5bd3a7cfd3fc74e5f6
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-30 01:33:24 +0530

    asynchandler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler=()=>{}
+// const asyncHandler=(fun)=>()=>{}
+// const asyncHandler=(fun)=>async()=>{}
+
+// const asyncHandler = (fn) => (req, res, next) => {
+//   try {
+//     await fn(req,res,next)
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
