45e1d924c4e140b92a1dd708714f18c8b8db1a71
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-30 01:56:39 +0530

    commit

---
+++ src/models/video.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String,
+      // cloudnary
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      // cloudnary
+      required: true,
+    },
+    title: {
+      type: String,
+
+      required: true,
+    },
+    description: {
+      type: String,
+
+      required: true,
+    },
+    duration: {
+      type: Number,
+
+      required: true,
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:false
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
