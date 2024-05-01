ffcad9b5cb589fddbfbd98a4dbc61df7aad04598
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-05-02 00:12:36 +0530

    commit

--- src/utils/cloudnery.js
+++ src/utils/cloudnery.js
@@ -1,4 +1,4 @@
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
 require("dotenv").config();
 
@@ -7,3 +7,24 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
+
+const uploadCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return null;
+    }
+    // upload the file to cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //   file uploaded  succesfully
+    console.log("file has been uploaded successfully", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the saved files as the the upload operations
+    console.log("error in uploading file to cloudinary", error);
+    return null;
+  }
+};
+
+export default uploadCloudinary;
